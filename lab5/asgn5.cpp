7080e1b18fd11993aa530a7397f040af94afae22
Sushant <hiraysushant@gmail.com>
2014-02-07 15:00:10 +0530

    Added initial files

---
+++ lab5/asgn5.cpp
@@ -0,0 +1,110 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <fstream>
+using namespace std;
+
+
+
+struct process_phase{
+	int iterations;
+	float cpu_time;
+	float io_time;
+};
+
+struct process{
+	int p_id;
+	int start_priority;
+	float admission;
+	vector<process_phase> phases;
+};
+
+struct sc_level{
+	int priority;
+	float time_slice;
+};
+
+struct scheduler{
+	int no_levels;
+	vector<sc_level> levels;
+};
+
+vector<process> process_list;
+scheduler my_scheduler;
+
+
+void process_proc_file(){
+	string line, line2;
+	int pid, prior;
+	float adm;
+	int iter;
+	float cpu_t, io_t;
+	ifstream infile("PROCESS_SPEC");
+	while (std::getline(infile, line))
+	{
+		if(line=="PROCESS"){
+			process proc;
+			getline(infile, line2);
+			std::istringstream iss(line2);
+		    if (!(iss >> pid >> prior >> adm)) { break; } // error
+		    // cout<<pid<<endl<<prior<<endl;
+			
+			proc.p_id = pid;
+			proc.start_priority = prior;
+			proc.admission = adm;
+
+			getline(infile, line2);
+			while(line2 != "END"){
+				std::istringstream iss(line2);
+				process_phase pp;
+			    if (!(iss >> iter >> cpu_t >> io_t)) { break; } // error
+			    // cout<<cpu_t<<endl<<io_t<<endl;
+			    
+			    pp.iterations = iter;
+			    pp.cpu_time = cpu_t;
+			    pp.io_time = io_t;
+			    (proc.phases).push_back(pp);
+			    getline(infile, line2);
+			}
+			process_list.push_back(proc);
+		}
+	}
+	// cout<<(process_list[1].phases[1]).io_time<<endl;
+	// return 0;
+}
+
+void process_scheduler_file(){
+	string line, line2;
+	int level_count;
+	int prior;
+	float t_slice;
+	ifstream infile("SCHEDULER_SPEC");
+	while (std::getline(infile, line))
+	{
+		if(line=="SCHEDULER"){
+			getline(infile, line2);
+			std::istringstream iss(line2);
+		    if (!(level_count)) { break; } // error
+		    // cout<<pid<<endl<<prior<<endl;
+			
+			my_scheduler.no_levels = level_count;
+			for(int i=0; i<level_count; i++){
+				getline(infile, line2);
+				std::istringstream iss(line2);
+				if (!(iss >> prior >> t_slice)) { break; } // error
+				sc_level scl;
+				scl.priority = prior;
+				scl.time_slice = t_slice;
+				my_scheduler.levels.push_back(scl);
+			}
+		}
+	}
+}
+
+int main(){
+	process_proc_file();
+	cout<<(process_list[1].phases[1]).io_time<<endl;
+	process_scheduler_file();
+	cout<<my_scheduler.levels[1].time_slice<<endl;
+}
